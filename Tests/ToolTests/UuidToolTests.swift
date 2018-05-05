2cc1648f2b546a9e9b88586c1cd298d8311a28a7
Keith Irwin <keith@zentrope.com>
2018-05-04 21:56:56 -0700

    Add testing placeholder

---
+++ Tests/ToolTests/UuidToolTests.swift
@@ -0,0 +1,11 @@
+import Foundation
+import XCTest
+import Tools
+
+class UidToolTests: XCTestCase {
+
+    func testTestingThing() throws {
+        // Placeholder for actual tests.
+        XCTAssertEqual(1, 1)
+    }
+}
